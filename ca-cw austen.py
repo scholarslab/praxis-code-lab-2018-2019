0d295357404b9dd28cbcc83140633d542263caf7
caddington11 <catherine.l.addington@gmail.com>
2018-10-29 10:37:37 -0400

    share week6 assignment1 w github

---
+++ ca-cw austen.py
@@ -0,0 +1,4 @@
+jane = 'The Project Gutenberg EBook of Pride and Prejudice, by Jane Austen This eBook is for the use of anyone anywhere at no cost and with almost no restrictions whatsoever. You may copy it, give it away or re-use it under the terms of the Project Gutenberg License included with this eBook or online at www.gutenberg.org Title: Pride and Prejudice Author: Jane Austen Posting Date: August 26, 2008 Release Date: June, 1998 Last Updated: March 10, 2018 Language: English Character set encoding: UTF-8 *** START OF THIS PROJECT GUTENBERG EBOOK PRIDE AND PREJUDICE *** Produced by Anonymous Volunteers PRIDE AND PREJUDICE By Jane Austen Chapter 1 It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.'
+jane_cleaned = jane.split('Produced by Anonymous Volunteers ')[1]
+jane_2018 = jane_cleaned.replace('man','person').replace('wife','partner')
+print(jane_2018)
