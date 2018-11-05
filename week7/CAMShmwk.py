a1d830519c597a84457bdfd3576027347b46ca5f
caddington11 <catherine.l.addington@gmail.com>
2018-11-05 10:04:02 -0500

    renamed

---
+++ week7/CAMShmwk.py
@@ -0,0 +1,12 @@
+#Create a class for storing information about historical figures.
+#Have the class store a first and last name. Have a method in the class to return the full name.
+#Have the class store the person's relevant century BUT there's only a set series of options for centuries, which is the following:
+#centuries = ['long 19th century', 'even longer 18th century', 'Medieval Times', '1999']
+#Have the class store the set options for centuries, and then have a method to check before adding a century if the century exist in centuries and only add it to the class if it's an accepted value.
+#Have the class store the person's expertises.
+#Have a method that makes sure any expertise added to expertises doesn't already exist in expertises.
+#Have a method to return the person's class information to a dictionary (hint checkout the classes resources for help!)
+#Use your class to create three historical figures.
+#Use that class three times to create three historical figures.
+#Print out all three figures information when you run the script.
+#Write the historical figures information to a text file using the python "io" library.
