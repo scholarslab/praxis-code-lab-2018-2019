5c2d353fd6dfcc48a298dc8e88817117da255e4c
caddington11 <catherine.l.addington@gmail.com>
2018-11-05 11:15:12 -0500

    progress so far

--- week7/supergrouphmwk.py
+++ week7/supergrouphmwk.py
@@ -1,11 +1,32 @@
-#Create a class for storing information about historical figures.
-#Have the class store a first and last name. Have a method in the class to return the full name.
-#Have the class store the person's relevant century BUT there's only a set series of options for centuries, which is the following:
-#centuries = ['long 19th century', 'even longer 18th century', 'Medieval Times', '1999']
-#Have the class store the set options for centuries, and then have a method to check before adding a century if the century exist in centuries and only add it to the class if it's an accepted value.
-#Have the class store the person's expertises.
-#Have a method that makes sure any expertise added to expertises doesn't already exist in expertises.
-#Have a method to return the person's class information to a dictionary (hint checkout the classes resources for help!)
+class historical_figures:
+    def __init__(self, first_name, last_name):
+       self.first_name = first_name
+       self.last_name = last_name
+       self.century = []
+       self.expertises = []
+    def get_full_name(self):
+        return self.first_name +' '+self.last_name
+    def set_century(self, century):
+        centuries = ['long 19th century','even longer 18th century', 'Medieval Times', '1999']
+        if century in centuries:
+            self.century = self.century + century
+        else:
+            pass
+    def add_expertises(self, expertises):
+        if expertises not in self.expertises:
+            self.expertises = self.expertises + expertises
+        else:
+            pass
+    def get_info(self):
+        self_to_dict = {
+            'full_name': self.first_name +' '+self.last_name, 'expertises': ', '.join(self.expertises), 'century': self.century
+        }
+        return self_to_dict
+
+hildie = historical_figures("Hildegard","Von Bingen")
+hildie.set_century(['Medieval Times'])
+hildie.add_expertises(['Jesus','music','science','languages'])
+print(hildie.get_info())
 #Use your class to create three historical figures.
 #Print out all three figures' information when you run the script.
 #Write the historical figures' information to a text file using the python "io" library.
