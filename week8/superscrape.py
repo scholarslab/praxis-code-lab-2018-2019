2f99fb11401b948c4fb86d0c1e0d573d1b1a3ff0
caddington11 <catherine.l.addington@gmail.com>
2018-11-12 11:41:08 -0500

    code so far!!

--- week8/superscrape.py
+++ week8/superscrape.py
@@ -5,7 +5,12 @@ result = requests.get('http://maybe.scholarslab.org/blog/')
 slab_blog = result.content
 soup = BeautifulSoup(slab_blog)
 
-blogtitles = soup.find_all("div",class_="blog-item__title")
-for title in blogtitles:
+firstblogtitles = soup.find_all("div",class_="blog-item__title")
+for title in firstblogtitles:
+    for link in title.find_all('a'):
+        print(link.get('href'))
+
+previousblogtitles = soup.find_all("ul",class_="page-previous_posts")
+for title in previousblogtitles:
     for link in title.find_all('a'):
         print(link.get('href'))
