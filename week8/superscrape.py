1ff4f4e30334518aefb134778241e675fe1802f0
caddington11 <catherine.l.addington@gmail.com>
2018-11-12 12:08:48 -0500

    homework finished

--- week8/superscrape.py
+++ week8/superscrape.py
@@ -5,12 +5,15 @@ result = requests.get('http://maybe.scholarslab.org/blog/')
 slab_blog = result.content
 soup = BeautifulSoup(slab_blog)
 
-firstblogtitles = soup.find_all("div",class_="blog-item__title")
-for title in firstblogtitles:
-    for link in title.find_all('a'):
-        print(link.get('href'))
-
-previousblogtitles = soup.find_all("ul",class_="page-previous_posts")
-for title in previousblogtitles:
-    for link in title.find_all('a'):
-        print(link.get('href'))
+f = open('blogpostlinks.txt','w')
+with open('blogpostlinks.txt','w') as file:
+    firstblogtitles = soup.find_all("div",class_="blog-item__title")
+    for title in firstblogtitles:
+        for link in title.find_all('a'):
+            initialresults = link.get('href')
+            print(initialresults, file=f)
+    previousblogtitles = soup.find_all("ul",class_="page-previous_posts")
+    for title in previousblogtitles:
+        for link in title.find_all('a'):
+            furtherresults = link.get('href')
+            print(furtherresults, file=f)
