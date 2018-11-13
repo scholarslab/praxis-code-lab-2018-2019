d26dd2191142461fa75780e66ff13a9a1ed9e34f
caddington11 <catherine.l.addington@gmail.com>
2018-11-13 11:15:01 -0500

    no redundant file object now!

--- week8/superscrape.py
+++ week8/superscrape.py
@@ -6,14 +6,13 @@ slab_blog = result.content
 soup = BeautifulSoup(slab_blog)
 
 f = open('blogpostlinks.txt','w')
-with open('blogpostlinks.txt','w') as file:
-    firstblogtitles = soup.find_all("div",class_="blog-item__title")
-    for title in firstblogtitles:
-        for link in title.find_all('a'):
-            initialresults = link.get('href')
-            print(initialresults, file=f)
-    previousblogtitles = soup.find_all("ul",class_="page-previous_posts")
-    for title in previousblogtitles:
-        for link in title.find_all('a'):
-            furtherresults = link.get('href')
-            print(furtherresults, file=f)
+firstblogtitles = soup.find_all("div",class_="blog-item__title")
+for title in firstblogtitles:
+    for link in title.find_all('a'):
+        initialresults = link.get('href')
+        print(initialresults, file=f)
+previousblogtitles = soup.find_all("ul",class_="page-previous_posts")
+for title in previousblogtitles:
+    for link in title.find_all('a'):
+        furtherresults = link.get('href')
+        print(furtherresults, file=f)
