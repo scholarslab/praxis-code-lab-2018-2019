5968a0b8d99bd343caa2ee7bc02b6b5a840a99c6
caddington11 <catherine.l.addington@gmail.com>
2018-12-03 12:12:24 -0500

    FOR LOOP

--- week10/lascasas.py
+++ week10/lascasas.py
@@ -7,6 +7,8 @@ result = requests.get('https://www.gutenberg.org/cache/epub/20321/pg20321.html')
 lascasas_text = result.content
 soup = BeautifulSoup(lascasas_text)
 
+#figure out how to get rid of the preface and start at p id=“id00015”
+
 #find all the italics
 allitalics = soup.find_all("i")
 
@@ -14,9 +16,9 @@ allitalics = soup.find_all("i")
 
 #write results to csv file
 with open('lascasasitalics.csv', 'w', newline='') as myfile:
-     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-     wr.writerow(allitalics)
-
-#this produces one long row instead of one long column
+     wr = csv.writer(myfile,delimiter=' ')
+     #should we add quoting=csv.QUOTE_ALL?
+     for italic in allitalics:
+        wr.writerow(italic)
 
 #figure out how to de-dupe the list
