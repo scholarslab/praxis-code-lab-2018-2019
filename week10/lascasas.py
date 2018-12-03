0e402a3deb63d85454f197fbcc93c1985af5673b
caddington11 <catherine.l.addington@gmail.com>
2018-12-03 11:54:13 -0500

    progress thus far

--- week10/lascasas.py
+++ week10/lascasas.py
@@ -2,10 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
+#get the text
 result = requests.get('https://www.gutenberg.org/cache/epub/20321/pg20321.html')
 lascasas_text = result.content
 soup = BeautifulSoup(lascasas_text)
 
-f = open('lascasasitalics.csv','w')
+#find all the italics
 allitalics = soup.find_all("i")
-print(allitalics, file=f)
+
+#figure out how to get rid of the <i> tags themselves
+
+#write results to csv file
+with open('lascasasitalics.csv', 'w', newline='') as myfile:
+     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+     wr.writerow(allitalics)
+
+#this produces one long row instead of one long column
+
+#figure out how to de-dupe the list
