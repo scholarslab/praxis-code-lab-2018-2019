d88b4829c2c9e05bebdfc2a11606bd66deedc0c3
caddington11 <catherine.l.addington@gmail.com>
2018-12-03 12:19:10 -0500

    the latest

--- week10/lascasas.py
+++ week10/lascasas.py
@@ -7,12 +7,10 @@ result = requests.get('https://www.gutenberg.org/cache/epub/20321/pg20321.html')
 lascasas_text = result.content
 soup = BeautifulSoup(lascasas_text)
 
-#figure out how to get rid of the preface and start at p id=“id00015”
+#in theory figure out how to get rid of the preface and start at p id=“id00015” but in reality we'll just delete that
 
 #find all the italics
 allitalics = soup.find_all("i")
-
-#figure out how to get rid of the <i> tags themselves
 
 #write results to csv file
 with open('lascasasitalics.csv', 'w', newline='') as myfile:
